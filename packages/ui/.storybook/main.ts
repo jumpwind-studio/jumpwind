a3380f87b9e14bf67eda7f4eb29b31af25b90d9b
timredd <timredd13@gmail.com>
2025-09-09 13:47:14 -0400

    update storybook config

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -2,15 +2,9 @@
 import type { StorybookConfig } from "storybook-solidjs-vite";
 import { mergeConfig } from "vite";
 
-function getAbsolutePath(value: string): any {
-  return value;
-  // return dirname(require.resolve(join(value, "../../../package.json")));
-}
-
 const config = {
   framework: "storybook-solidjs-vite",
   addons: [
-    "@storybook/addon-onboarding",
     "@storybook/addon-docs",
     "@storybook/addon-a11y",
     "@storybook/addon-links",
@@ -23,8 +17,8 @@ const config = {
     },
   ],
   stories: [
-    getAbsolutePath("../stories/**/*.mdx"),
-    getAbsolutePath("../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"),
+    "../registry/jumpwind/stories/**/*.mdx",
+    "../registry/jumpwind/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   core: {
     builder: "@storybook/builder-vite", // 👈 The builder enabled here.
@@ -43,7 +37,6 @@ const config = {
     reactDocgen: "react-docgen-typescript",
     reactDocgenTypescriptOptions: {
       shouldExtractLiteralValuesFromEnum: true,
-      // 👇 Default prop filter, which excludes props from node_modules
       propFilter: (prop: any) =>
         prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
     },
--- packages/ui/.storybook/preview.ts
+++ packages/ui/.storybook/preview.ts
@@ -5,18 +5,7 @@ import type { Preview } from "storybook-solidjs-vite";
 import "../registry/jumpwind/styles/styles.css";
 
 const preview = {
-  decorators: [
-    withThemeByDataAttribute({
-      attributeName: "data-mode",
-      defaultTheme: "dark",
-      themes: {
-        light: "light",
-        dark: "dark",
-      },
-    }),
-  ],
   tags: ["autodocs"],
-  args: { theme: "dark" },
   parameters: {
     actions: {
       // create action args for all props that start with "on"
@@ -31,13 +20,24 @@ const preview = {
     docs: {
       codePanel: true,
       controls: {
-        exclude: ["ref"],
+        exclude: ["ref", "children"],
         sort: "requiredFirst",
       },
       theme: themes.dark,
     },
     layout: "centered",
   },
+  decorators: [
+    withThemeByDataAttribute({
+      attributeName: "data-mode",
+      defaultTheme: "dark",
+      themes: {
+        light: "light",
+        dark: "dark",
+      },
+    }),
+  ],
+  args: { theme: "dark" },
 } satisfies Preview;
 
 export default preview;
