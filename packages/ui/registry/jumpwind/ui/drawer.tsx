6968799e6b61515295fe9ccef5ff3f9ccd7edd1e
timredd <timredd13@gmail.com>
2025-09-09 18:45:10 -0400

    update drawer

--- packages/ui/registry/jumpwind/ui/drawer.tsx
+++ packages/ui/registry/jumpwind/ui/drawer.tsx
@@ -24,17 +24,19 @@ function DrawerClose(props: ComponentProps<typeof DrawerPrimitive.Close>) {
 function DrawerOverlay(props: ComponentProps<typeof DrawerPrimitive.Overlay>) {
   const [local, rest] = splitProps(props, ["class"]);
 
-  const drawer = useDrawer();
+  const { openPercentage } = useDrawer();
 
   return (
     <DrawerPrimitive.Overlay
       data-slot="drawer-overlay"
       class={cn(
-        "fixed inset-0 z-50 bg-black/50 data-closed:animate-out data-open:animate-in data-transitioning:transition-colors data-transitioning:duration-300",
+        "fixed inset-0 z-50 bg-black/50",
+        "data-closed:animate-out data-open:animate-in",
+        "data-transitioning:transition-colors data-transitioning:duration-300",
         local.class,
       )}
       style={{
-        "background-color": `rgb(0 0 0 / ${0.8 * drawer.openPercentage()})`,
+        "background-color": `rgb(0 0 0 / ${0.8 * openPercentage()})`,
       }}
       {...rest}
     />
@@ -51,10 +53,10 @@ function DrawerContent(props: ComponentProps<typeof DrawerPrimitive.Content>) {
         data-slot="drawer-content"
         class={cn(
           "group/drawer-content fixed z-50 flex h-auto flex-col bg-background",
-          "data-[side=top]:inset-x-0 data-[side-direction=top]:top-0 data-[side-direction=top]:mb-24 data-[side-direction=top]:max-h-[80vh] data-[side-direction=top]:rounded-b-lg data-[side-direction=top]:border-b",
-          "data-[top=bottom]:inset-x-0 data-[side-direction=bottom]:bottom-0 data-[side-direction=bottom]:mt-24 data-[side-direction=bottom]:max-h-[80vh] data-[side-direction=bottom]:rounded-t-lg data-[side-direction=bottom]:border-t",
-          "data-[top=right]:inset-y-0 data-[side-direction=right]:right-0 data-[side-direction=right]:w-3/4 data-[side-direction=right]:border-l data-[side-direction=right]:sm:max-w-sm",
-          "data-[top=left]:inset-y-0 data-[side-direction=left]:left-0 data-[side-direction=left]:w-3/4 data-[side-direction=left]:border-r data-[side-direction=left]:sm:max-w-sm",
+          "data-[side=top]:inset-x-0 data-[side=top]:top-0 data-[side=top]:mb-24 data-[side=top]:max-h-[80vh] data-[side=top]:rounded-b-lg data-[side=top]:border-b",
+          "data-[side=bottom]:inset-x-0 data-[side=bottom]:bottom-0 data-[side=bottom]:mt-24 data-[side=bottom]:max-h-[80vh] data-[side=bottom]:rounded-t-lg data-[side=bottom]:border-t",
+          "data-[side=right]:inset-y-0 data-[side=right]:right-0 data-[side=right]:w-3/4 data-[side=right]:border-l data-[side=right]:sm:max-w-sm",
+          "data-[side=left]:inset-y-0 data-[side=left]:left-0 data-[side=left]:w-3/4 data-[side=left]:border-r data-[side=left]:sm:max-w-sm",
           local.class,
         )}
         {...rest}
