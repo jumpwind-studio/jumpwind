a839fa9fbac1331b211130a6a04acd644427bffc
timredd <timredd13@gmail.com>
2025-09-11 14:31:29 -0400

    move files from registry to src

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,3 @@
-export * from "../registry/jumpwind/lib/utils.js";
-export * from "../registry/jumpwind/ui/index.js";
+export * from "./lib/utils.js";
 export * as Registry from "./registry/service.js";
+export * from "./ui/index.js";
